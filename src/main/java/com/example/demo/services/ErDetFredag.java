132ce18a3b8549107a554c4324f5b5f94daebfa8
Tobias <Tobias.as@hotmail.com>
2020-09-08 15:31:47 +0200

    Færdig version

--- src/main/java/com/example/demo/services/ErDetFredag.java
+++ src/main/java/com/example/demo/services/ErDetFredag.java
@@ -6,10 +6,10 @@ import java.util.Date;
 
 
 public class ErDetFredag {
-    double q = 8;
-    double m = 9;
-    double j = 2020/100;
-    double k = 2020%100;
+    double q = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+    double m = Calendar.getInstance().get(Calendar.MONTH) + 1;
+    double j = Calendar.getInstance().get(Calendar.YEAR)/100;
+    double k = Calendar.getInstance().get(Calendar.YEAR)%100;
 
     double h = ( q + Math.floor( ( 13 * ( m + 1 ) ) / 5 ) + k + Math.floor( k / 4 ) + Math.floor( j / 4) - 2 * j ) % 7;
     double d = ( (h + 5) % 7 ) + 1;
